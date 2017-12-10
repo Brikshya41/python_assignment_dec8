f47e0fdfa2cc4265df118c4a29855a4870ee8205
Brikshya41 <brikshyadhungana@gmail.com>
2017-12-10 21:29:10 +0545

    python assignment

---
+++ password.py
@@ -0,0 +1,12 @@
+password="class"
+
+def id(value):
+    if(value==password):
+        print("password match")
+
+    else:
+        print("wrong password")
+
+
+value=input("Enter your password:")
+id(value)
